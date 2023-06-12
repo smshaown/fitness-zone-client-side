ccf358372eefd3096b940b01f5444a68869bdeed
smshaown <smshaown65@gmail.com>
2023-06-12 14:29:20 +0600

    done the home section

--- src/Page/SingUp/SingUp.jsx
+++ src/Page/SingUp/SingUp.jsx
@@ -145,7 +145,7 @@ const SignUp = () => {
                     Email address
                   </label>
                   <input
-                    style={{ borderRadius: "0px 200px 0px 200px" }}
+                    // style={{ borderRadius: "0px 200px 0px 200px" }}
                     type="email"
                     name="email"
                     id="email"
