1cbeb8bfdcf44c5c96e183bf8a2441aa302ace82
smshaown <smshaown65@gmail.com>
2023-06-12 12:56:19 +0600

    extra section add

--- src/Page/FullTimeCurse/FullTimeCurse.jsx
+++ src/Page/FullTimeCurse/FullTimeCurse.jsx
@@ -5,7 +5,7 @@ const FullTimeCurse = () => {
         <div>
             <h2 className='mt-12 mb-12 font-bold'>Full Time Courses</h2>
             <div>
-                
+                <div></div>                 
             </div>
         </div>
     );
