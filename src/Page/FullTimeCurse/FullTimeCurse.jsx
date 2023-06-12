cdfd81e95405cb2f1ffa4a5e15ad78a6b03cad07
smshaown <smshaown65@gmail.com>
2023-06-12 12:51:31 +0600

    full time cures add .

---
+++ src/Page/FullTimeCurse/FullTimeCurse.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FullTimeCurse = () => {
+    return (
+        <div>
+            <h2 className='mt-12 mb-12 font-bold'>Full Time Courses</h2>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default FullTimeCurse;
